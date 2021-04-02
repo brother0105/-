9589d184532449e3e7c11f6705c99e00a8ebdc6e
kjb421 <80325407+kjb421@users.noreply.github.com>
2021-04-02 23:19:13 +0900

    Add files via upload

---
+++ 진범/SubActivity02.java
@@ -0,0 +1,13 @@
+package com.example.learning;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SubActivity02 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub02);
+    }
+}
