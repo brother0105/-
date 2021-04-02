1fc7120ab4ffe826a05f7e3e4a41c93c7fa89590
meltnia <72488905+meltnia@users.noreply.github.com>
2021-04-02 16:03:51 +0900

    Add files via upload

---
+++ makeafood/app/src/main/java/com/example/makeafood/Myfavnview.java
@@ -0,0 +1,18 @@
+package com.example.makeafood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+
+public class Myfavnview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
