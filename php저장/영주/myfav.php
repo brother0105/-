6ab995efd9983d7d46caae03268dec98cadcb56d
meltnia <72488905+meltnia@users.noreply.github.com>
2021-06-26 02:02:32 +0900

    Add files via upload

---
+++ php저장/영주/myfav.php
@@ -0,0 +1,33 @@
+<?php 
+
+    error_reporting(E_ALL); 
+    ini_set('display_errors',1); 
+
+    include('dbcon.php');
+        
+
+    $stmt = $con->prepare('select * from recipe AS rec JOIN recipe_favorite AS rfav ON rec.recipe_num=rfav.recipe_num');
+    $stmt->execute();
+
+    if ($stmt->rowCount() > 0)
+    {
+        $data = array(); 
+
+        while($row=$stmt->fetch(PDO::FETCH_ASSOC))
+        {
+            extract($row);
+    
+            array_push($data, 
+                array(
+				'recphoto'=>$photo,
+                'recname'=>$recipe_name,
+                'memo'=>$f_memo
+            ));
+        }
+
+        header('Content-Type: application/json; charset=utf8');
+        $json = json_encode(array("tester"=>$data), JSON_PRETTY_PRINT+JSON_UNESCAPED_UNICODE);
+        echo $json;
+    }
+
+?>
