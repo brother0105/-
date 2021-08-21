d0ddd33cd3573f2e73dfc557b98843d4cd482f85
meltnia <72488905+meltnia@users.noreply.github.com>
2021-08-22 02:32:39 +0900

    Add files via upload

--- php저장/영주/myfav.php
+++ php저장/영주/myfav.php
@@ -6,7 +6,7 @@
     include('dbcon.php');
         
 
-    $stmt = $con->prepare('select * from recipe AS rec JOIN recipe_favorite AS rfav ON rec.recipe_num=rfav.recipe_num');
+    $stmt = $con->prepare('select r_fav.recipe_num, recipe_name, photo, f_memo from recipe_favorite AS r_fav left outer join recipe AS rec on r_fav.recipe_num = rec.recipe_num;');
     $stmt->execute();
 
     if ($stmt->rowCount() > 0)
